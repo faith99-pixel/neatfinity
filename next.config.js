4f32c72249c4151e620e4a8b562b75e32c2390b1
Olanrewaju-tech <akejusolomonolanrewaju@gmail.com>
2026-01-13 18:26:20 +0100

    Add GitHub Actions deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
 
 
 // next.config.js
